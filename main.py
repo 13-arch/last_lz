fed33e12e6e01985a8e95eb87f47979726dea5f1
13-arch <Iron_Mike@tutamail.com>
2024-12-31 10:25:50 +0300

    hhh9

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ import Archimed as A
 
 # The main function of the project
 def main():
-    user_choise = int(input("Выберите функцию, которую хотите использовать: "))
+    user_choise = int(input("Выберите функцию, которую хотите использовать 1 или 2: "))
     if user_choise == 1:
         base_1 = float(input("Введите площадь нижнего основания "))
         base_2 = float(input("Введите площадь верхнего основания "))
         height = float(input("Введите высоту усечённой пирамиды "))
         print(f"ОбЪём усечённой пирамиды составляет {p.Value(base_1, base_2, height)} кубических единиц")
-    if user_choise == 2:
-        mass = float(input("Введите массу "))
-        volume = float(input("Введите объём погружённой части"))
-        density = float(input("Введите плотность "))
-        A.Counting(mass, volume, density)
+    elif user_choise == 2:
+        mass = float(input("Введите массу: "))
+        volume = float(input("Введите объём погружённой части: "))
+        density = float(input("Введите плотность: "))
+        A.plav(mass, volume, density)
+    else:
+        print('выберите из 1 или 2')
 
 
 
